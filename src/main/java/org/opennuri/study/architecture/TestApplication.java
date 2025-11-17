4a0b2224528969b3169bbfb5099e02f82a56f0ac
roadseeker <kyoungik.lee@gmail.com>
2025-11-17 18:54:51 +0900

    initial commit

---
+++ src/main/java/org/opennuri/study/architecture/TestApplication.java
@@ -0,0 +1,13 @@
+package org.opennuri.study.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
